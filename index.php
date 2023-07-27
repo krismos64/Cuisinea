cbf9205e67574261d55d383162d68026d81a27c1
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-07-27 17:19:06 +0200

    correction css

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
     <div class="col-md-4">
       <div class="card">
-        <img src="uploads/recipes/1-chocolate-au-mousse.jpg" class="card-img-top" alt="...">
+        <img src="uploads/recipes/2-gratin-dauphinois.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -77,7 +77,7 @@
 
       <div class="col-md-4">
       <div class="card">
-        <img src="uploads/recipes/1-chocolate-au-mousse.jpg" class="card-img-top" alt="...">
+        <img src="uploads/recipes/3-salade.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
