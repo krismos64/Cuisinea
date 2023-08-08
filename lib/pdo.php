4ba19e7e4d6774b75f2cb1b9d5273c27d4ffa761
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-08-08 09:48:05 +0200

    database pdo

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,3 +1,34 @@
 <?php
+class Database
+{
+    private static $dbHost = "mysql-krismos64.alwaysdata.net";
+    private static $dbName = "krismos64_cuisinea";
+    private static $dbUsername = "krismos64";
+    private static $dbUserpassword = "Mostefaoui1";
+    
+    private static $connection = null;
+    
+    public static function connect()
+    {
+        if(self::$connection == null)
+        {
+            try
+            {
+              self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUsername, self::$dbUserpassword);
+            }
+            catch(PDOException $e)
+            {
+                die($e->getMessage());
+            }
+        }
+        return self::$connection;
+    }
+    
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
 
-$pdo = new PDO("mysql:dbname=krismos64_cuisinea;host=mysql-krismos64.alwaysdata.net;charset=utf8mb4;username=krismos64;password=Mostefaoui1");
+}
+
+?>
